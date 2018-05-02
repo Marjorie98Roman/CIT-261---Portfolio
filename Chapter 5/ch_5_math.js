916a54a84c8e3446b43e9025528be4af64bd1301
Marjorie98Roman <32476794+Marjorie98Roman@users.noreply.github.com>
2018-05-02 03:18:08 -0600

    Added new files to the Chapter 5 assignments

---
+++ Chapter 5/ch_5_math.js
@@ -0,0 +1,54 @@
+Math.PI // The ratio of the circumference and diameter of a circle
+<< 3.141592653589793
+
+Math.SQRT2 // The square root of 2
+<< 1.4142135623730951
+
+Math.SQRT1_2 // The reciprocal of the square root of 2
+<< 0.7071067811865476
+
+Math.E // Euler's constant
+<< 2.718281828459045
+
+Math.LN2 // The natural logarithm of 2
+<< 0.6931471805599453
+
+Math.LN10 // The natural logarithm of 10
+<< 2.302585092994046
+
+Math.LOG2E // Log base 2 of Euler's constant
+<< 1.4426950408889634
+
+Math.LOG10E // Log base 10 of Euler's constant
+<< 0.4342944819032518
+
+
+Math.ceil(4.2);
+<< 5
+
+Math.ceil(8);
+<< 8
+
+Math.ceil(-4.2);
+<< -4
+
+
+Math.exp(1); // This is Euler's constant
+<< 2.718281828459045
+
+Math.exp(0); // Any number to the power of 0 is 1
+<< 1
+
+Math.exp(-3);
+<< 0.049787068367863944
+
+
+Math.sqrt(121);
+<< 11
+
+Math.sqrt(2); // same as Math.SQRT2
+<< 1.4142135623730951
+
+Math.sqrt(-1); // imaginary numbers aren't supported!
+<< NaN
+
