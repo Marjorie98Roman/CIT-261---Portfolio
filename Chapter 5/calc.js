c917628e28594dbebd1ee8c9f3f04af5683c0d59
Marjorie98Roman <32476794+Marjorie98Roman@users.noreply.github.com>
2018-05-05 18:29:02 -0600

    Added Ch 7 Assignments

--- Chapter 5/calc.js
+++ Chapter 5/calc.js
@@ -1,24 +1,34 @@
-function buttonClicked(button) {
-  console.log(button.innerHTML);
-  let inputBox = document.getElementById('calcInput');
+const calculator = {
+    number1 : 0,
+    number2 : 0,
+    operation : '',
+    inputOutput: document.getElementById('calcInput'),
+    clear : function () {
+        this.inputOutput.value = '';
+    },
+    buttonClicked : function (button) {
+      console.log(button.innerHTML);
+      let inputBox = document.getElementById('calcInput');
 
-  switch (button.innerHTML) {
-    case 'M':
-      break;
-    case 'C':
-      break;
-    case '/':
-      break;
-    case 'X':
-      break;
-    case '-':
-      break;
-    case '+':
-      break;
-    case '=':
-      break;
-    default:
-      //if it made it to here it's a number
-      inputBox.value = inputBox.value + button.innerHTML;
-  }
+      switch (button.innerHTML) {
+        case 'M':
+          break;
+        case 'C':
+            this.clear();  
+          break;
+        case '/':
+          break;
+        case 'X':
+          break;
+        case '-':
+          break;
+        case '+':
+          break;
+        case '=':
+          break;
+        default:
+          //if it made it to here it's a number
+          inputBox.value = inputBox.value + button.innerHTML;
+        }
+    }
 }
---
+++ Chapter 7/main.js
@@ -0,0 +1,107 @@
+function doSomething(){
+    console.log('Something Happened!');
+}
+addEventListener('click', doSomething);
+
+function doSomething(event){
+    console.log(event.type);
+}
+
+function doSomething(event){
+    console.log(event.target);
+}
+
+function doSomething(event){
+    console.log(`screen: (${event.screenX},${event.screenY}), page: (${event.pageX},${event.pageY}), client: (${event.screenX},${event.screenY})`)
+}
+
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
+
+mouseParagraph.addEventListener('mousemove', () => console.log('You Moved!') );
+
+addEventListener('keydown',highlight);
+addEventListener('keyup', (event) => console.log(`You stopped pressing the key on ${new Date}`));
+
+addEventListener('keypress', (event) => console.log(`You pressed the ${event.key} character`));
+
+addEventListener('keydown', (event) => console.log(`You pressed the ${event.key} character`));
+
+addEventListener('keydown', (event) => {
+    if (event.key === 'c' && event.ctrlKey) {
+        console.log('Action canceled!');
+    }
+});
+
+addEventListener('click', (event) => {
+    if (event.shiftKey) {
+        console.log('A Shifty Click!');
+    }
+});
+
+addEventListener('touchend', () => console.log('Touch stopped');
+
+const onceParagraph = document.getElementById('once');
+onceParagraph.addEventListener('click', remove);
+function remove(event) {
+    console.log('Enjoy this while it lasts!');
+    onceParagraph.style.backgroundColor = 'pink';
+    onceParagraph.removeEventListener('click',remove);
+}
+
+const brokenLink = document.getElementById('broken');
+brokenLink.addEventListener('click',(event) => {
+    event.preventDefault();
+    console.log('Broken Link!');
+});
+
+ulElement = document.getElementById('list');
+liElement = document.querySelector('#list li');
+
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul') );
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li') );
+
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
+
+// capturing
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),true);
+
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),true);
+
+// bubbling
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul'),false );
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li'),false );
+
+liElement.addEventListener('click', (event) => {
+console.log('clicked on li');
+event.stopPropagation(); }, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
